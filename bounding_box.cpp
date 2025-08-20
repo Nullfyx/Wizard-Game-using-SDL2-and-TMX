019a984add8ecbefe7ca371fb214827aa80faa42
nullfyx <taranveersinghpunjabi@gmail.com>
2025-08-20 15:53:29 +0530

    minor bug fix

--- bounding_box.cpp
+++ bounding_box.cpp
@@ -1,36 +1,34 @@
-#include"bounding_box.hpp"
+#include "bounding_box.hpp"
 #include "globals.hpp"
 #include <SDL2/SDL_render.h>
 
-bool checkCollisionB(SDL_Rect &a, SDL_Rect &b)
-{
- //   SDL_SetRenderDrawColor(wRenderer, 255, 0, 0, 255);
+bool checkCollisionB(SDL_Rect &a, SDL_Rect &b) {
+  //   SDL_SetRenderDrawColor(wRenderer, 255, 0, 0, 255);
   //  SDL_RenderDrawRect(wRenderer, &a);
-   // SDL_RenderDrawRect(wRenderer, &b);
-    //SDL_RenderPresent(wRenderer);
-    // Calculate the sides of rect A
-    int leftA = a.x;
-    int rightA = a.x + a.w;
-    int topA = a.y;
-    int bottomA = a.y + a.h;
+  // SDL_RenderDrawRect(wRenderer, &b);
+  // SDL_RenderPresent(wRenderer);
+  // Calculate the sides of rect A
+  int leftA = a.x;
+  int rightA = a.x + a.w;
+  int topA = a.y;
+  int bottomA = a.y + a.h;
 
-    // Calculate the sides of rect B
-    int leftB = b.x;
-    int rightB = b.x + b.w;
-    int topB = b.y;
-    int bottomB = b.y + b.h;
+  // Calculate the sides of rect B
+  int leftB = b.x;
+  int rightB = b.x + b.w;
+  int topB = b.y;
+  int bottomB = b.y + b.h;
 
-    // Check if there is no overlap
-    if (bottomA <= topB)
-        return false; // A is above B
-    if (topA >= bottomB)
-        return false; // A is below B
-    if (rightA <= leftB)
-        return false; // A is left of B
-    if (leftA >= rightB)
-        return false; // A is right of B
+  // Check if there is no overlap
+  if (bottomA <= topB)
+    return false; // A is above B
+  if (topA >= bottomB)
+    return false; // A is below B
+  if (rightA <= leftB)
+    return false; // A is left of B
+  if (leftA >= rightB)
+    return false; // A is right of B
 
-    // If none of the above, there's a collision
-    return true;
+  // If none of the above, there's a collision
+  return true;
 }
-
--- player.hpp
+++ player.hpp
@@ -1,171 +1,171 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
+#include "Kinematics.hpp"
+#include "collisions.hpp"
 #include "globals.hpp"
-#include <iostream>
-#include "map.hpp"
 #include "gravity.hpp"
+#include "map.hpp"
+#include "mapglobal.hpp"
 #include "projectile.hpp"
 #include "texture.hpp"
 #include "timer.hpp"
+#include <iostream>
 #include <tmx.h>
-#include "collisions.hpp"
-#include "Kinematics.hpp"
-#include"mapglobal.hpp"
 using namespace std;
 
-class Player : public Kinematics
-{
-    // width, height of player
-    int wWidth, wHeight;
+class Player : public Kinematics {
+  // width, height of player
+  int wWidth, wHeight;
 
-    // weight of the player
-    double wWeight;
+  // weight of the player
+  double wWeight;
 
-    // max velocity of the player
-    double wMaxVel;
+  // max velocity of the player
+  double wMaxVel;
 
-    // ATK of the player
-    double wAtk;
+  // ATK of the player
+  double wAtk;
 
-    // health of the player
-    int wHealth;
+  // health of the player
+  int wHealth;
 
-    // lives of the player
-    int wLives;
+  // lives of the player
+  int wLives;
 
-    // the x position anf=d y position of player w.r.t screen
-    int wXPos;
-    int wYPos;
+  // the x position anf=d y position of player w.r.t screen
+  int wXPos;
+  int wYPos;
 
-    // the x and y velocities
-    int wXVel;
-    int wYVel;
+  // the x and y velocities
+  int wXVel;
+  int wYVel;
 
-    // velocity due to gravity
-    double wGravityVel;
+  // velocity due to gravity
+  double wGravityVel;
 
-    // rectangle of the player
-    SDL_Rect playerRect = {wXPos, wYPos, wWidth, wHeight};
+  // rectangle of the player
+  SDL_Rect playerRect = {wXPos, wYPos, wWidth, wHeight};
 
-    // flip
-    SDL_RendererFlip flip;
+  // flip
+  SDL_RendererFlip flip;
 
-    // detlatime
-    float dt;
+  // detlatime
+  float dt;
 
-    // rotate
-    double rotate;
+  // rotate
+  double rotate;
 
-    // jump power
-    double wJumpPower;
+  // jump power
+  double wJumpPower;
 
-    // player map
-    tmx_map *map;
+  // player map
+  tmx_map *map;
 
-    // isGrounded
-    double isGrounded;
+  // isGrounded
+  double isGrounded;
 
-    // check if the player is jumping
-    bool jumping;
+  // check if the player is jumping
+  bool jumping;
 
 public:
-    // constructor
-    Player();
-    double cooldown;
-    double accDt;
-    bool isRoofed;
-    bool isRightObstructed;
-    bool isLeftObstructed;
-    // getters and setters
-    int width();
+  // constructor
+  Player();
+  double cooldown;
+  double accDt;
+  bool isRoofed;
+  bool isRightObstructed;
+  bool isLeftObstructed;
+  // getters and setters
+  int width();
 
-    int height();
+  int height();
 
-    double weight();
+  double weight();
 
-    double maxVel();
+  double maxVel();
 
-    double atk();
+  double atk();
 
-    int health();
+  int health();
 
-    int lives();
+  int lives();
 
-    int xPos();
+  int xPos();
 
-    int yPos();
+  int yPos();
 
-    int xVel();
+  int xVel();
 
-    int yVel();
+  int yVel();
 
-    double jumpPower();
+  double jumpPower();
 
-    double gravityVel();
+  double gravityVel();
 
-    SDL_Rect *getCollider();
+  SDL_Rect *getCollider();
 
-    auto getMap();
+  auto getMap();
 
-    bool getisGrounded();
+  bool getisGrounded();
 
-    bool isJumping();
+  bool isJumping();
 
-    void setWidth(int w);
+  void setWidth(int w);
 
-    void setHeight(int h);
+  void setHeight(int h);
 
-    void setWeight(double w);
+  void setWeight(double w);
 
-    void setMaxVel(double m);
+  void setMaxVel(double m);
 
-    void setAtk(double a);
+  void setAtk(double a);
 
-    void setHealth(int h);
+  void setHealth(int h);
 
-    void setLives(int l);
+  void setLives(int l);
 
-    void setXPos(int x);
+  void setXPos(int x);
 
-    void setYPos(int y);
+  void setYPos(int y);
 
-    void setXVel(int x);
+  void setXVel(int x);
 
-    void setYVel(int y);
+  void setYVel(int y);
 
-    void setCollider(SDL_Rect p);
+  void setCollider(SDL_Rect p);
 
-    void setFlip(SDL_RendererFlip f);
+  void setFlip(SDL_RendererFlip f);
 
-    void setRotate(double a);
+  void setRotate(double a);
 
-    void setGravityVel(double g);
+  void setGravityVel(double g);
 
-    void setJumpPower(double p);
+  void setJumpPower(double p);
 
-    void setMap(tmx_map *map);
+  void setMap(tmx_map *map);
 
-    void setisGrounded(bool u);
+  void setisGrounded(bool u);
 
-    void setJumping(bool j);
+  void setJumping(bool j);
 
-    // move the player
-    void moveRender(bool moveRight, bool moveLeft, bool jump, bool attack, vector<projectile*> &projectiles);
+  // move the player
+  void moveRender(bool moveRight, bool moveLeft, bool jump, bool attack,
+                  vector<projectile *> &projectiles);
 
-    // update the time;
-    void update(float d);
+  // update the time;
+  void update(float d);
 
-    // the texture of the player
-    WTexture playerTexture;
+  // the texture of the player
+  WTexture playerTexture;
 
-    // gravity
-    Gravity gravity;
+  // gravity
+  Gravity gravity;
 
-    // collision detection
-    bool isTileCollidableY();
-    bool isTileCollidableX();
+  // collision detection
+  bool isTileCollidableY();
+  bool isTileCollidableX();
 
-    // jump
-    void jump();
+  // jump
+  void jump();
 };
 #endif
